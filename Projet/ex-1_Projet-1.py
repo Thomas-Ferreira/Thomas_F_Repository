c37e78ece65f07c682100ecd3c976c740a596422
Thomas-Ferreira <thomas.ferreira80@gmail.com>
2020-09-03 18:06:25 +0200

    modif projet

--- Projet/ex-1_Projet-1.py
+++ Projet/ex-1_Projet-1.py
@@ -4,12 +4,14 @@ import json
 
 
 def getRepositories():
-	reponse = requests.get("https://api.github.com/repositories")
-	#parser = BeautifulSoup(results.text)
-	if reponse:
-		print('Success')
-	else:
-		print('Error')
+	res = requests.get("https://api.github.com/repositories")
+	#parser = BeautifulSoup(reponse.text)
+	json.loads(res.text)
+	
+	json_item = json.loads(res.text)
+	for item in json_item:
+		title = "Title : " + item["name"]
+		open("/home/thomas/Thomas_F_Repository/Projet/results.text","a").write(title)
 		
 
 getRepositories()	
