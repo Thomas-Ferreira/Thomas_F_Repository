77f472fb78de0482e1720dc50aafbb745314cf91
Thomas-Ferreira <thomas.ferreira80@gmail.com>
2020-09-04 10:21:00 +0200

    ex_2 + fi ex_1

--- Projet/ex-1_Projet-1.py
+++ Projet/ex-1_Projet-1.py
@@ -1,17 +1,23 @@
+import os
 import requests
 from bs4 import BeautifulSoup
 import json
-
+		
+#def emptyfile(fichier.text)
+	#if fichier.text is not none
+		
 
 def getRepositories():
 	res = requests.get("https://api.github.com/repositories")
-	#parser = BeautifulSoup(reponse.text)
 	json.loads(res.text)
 	
 	json_item = json.loads(res.text)
 	for item in json_item:
-		title = "Title : " + item["name"]
-		open("/home/thomas/Thomas_F_Repository/Projet/results.text","a").write(title+"\n")
+		title = "Title : " + item["name"] + "\n" + "Description : "
+		des = item["description"]
+		if des is None :
+			des = "no description "
+		open("/home/thomas/Thomas_F_Repository/Projet/results.text","a").write(title+des+"\n")
 		
 
 getRepositories()	
